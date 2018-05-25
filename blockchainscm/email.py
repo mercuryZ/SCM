669d9c3f36856a1b4896203dd9dbb7b5244984fb
mercuryZ <15210820708@163.com>
2018-05-25 15:54:07 +0800

    fix some

--- blockchainscm/email.py
+++ blockchainscm/email.py
@@ -11,8 +11,8 @@ def send_asyn_email(app, msg):
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg = Message(app.config['FLASK_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASK_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt' ,**kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_asyn_email, args=[app, msg])
--- manage.py
+++ manage.py
@@ -1,1 +1,21 @@
 # -*-coding:utf-8 -*-
+import os
+from flask_migrate import Migrate
+from blockchainscm import create_app, db
+from blockchainscm.models import User, Role
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+@app.cli.command()
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestResult(verbosity=2).run(tests)
+
